b55b849ed20ab7409b22f9bfcc2c691a07ce9b14
Harini B <harinib2703@gmail.com>
2026-09-21 12:15:29 +0530

    Implement main method in Rotate Array class
    Added main method for testing array rotation.

---
+++ Rotate Array.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        int[] rotated=new int[n];
+        for(int i=0;i<n;i++){
+            rotated[(i+k)%n]=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=rotated[i];      
+        }
+    }
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[] nums=new int[n];
+    for(int i=0;i<n;i++){
+      nums[i]=sc.nextint();
+    }
+    int k=sc.nextInt();
+    Solution sol=new Solution();
+    int[] result=sol.rotate(nums,k);
+    System.out.println(Arrays.toString(result));
+  }
+}
