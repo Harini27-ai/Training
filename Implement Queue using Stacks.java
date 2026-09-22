72992488ed878887c6a51739bd04d23967a51dc8
Harini B <harinib2703@gmail.com>
2026-09-22 09:13:33 +0530

    Clean up imports in Queue implementation
    Removed unused imports for cleaner code.

--- Implement Queue using Stacks.java
+++ Implement Queue using Stacks.java
@@ -1,7 +1,4 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     static class MyQueue {
         private Deque<Integer> input = new ArrayDeque<>();
