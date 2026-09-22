38b062e47262850e595e6ab590a83740d331cdd3
Harini B <harinib2703@gmail.com>
2026-09-22 08:47:37 +0530

    Add Valid Parentheses solution in Java
    Implement a solution to validate parentheses using a stack.

---
+++ Valid Parentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                int top=st.pop();
+                if((ch==')' && top!='(') || (ch==']' && top!='[') || (ch=='}' && top!='{')){
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLinr();
+    Solution sol=new Solution();
+    boolean result=sol.isValid(s);
+    System.out.println(result);
+  }
+}
